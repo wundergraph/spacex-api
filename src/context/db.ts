d5ce05bceb4eb93fb8410dc110ad56059d226503
starptech <deusdustin@gmail.com>
2022-09-05 15:45:00 +0200

    upgrade deps

--- src/context/db.ts
+++ src/context/db.ts
@@ -1,15 +1,21 @@
 /*eslint-disable */
-import MongoClient from 'mongodb';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
 const dbPass = process.env.DB_PASS
-const url =
-  `mongodb+srv://wg:${dbPass}@spacex.loqbbog.mongodb.net/spacex-api`;
+
+const uri = `mongodb+srv://wg:${dbPass}@spacex.loqbbog.mongodb.net/?retryWrites=true&w=majority`;
 
 export const getDB = async () => {
-  const client = await MongoClient.connect(
-    url,
-    { poolSize: 20, useNewUrlParser: true }
-  );
+  const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+  try {
+    console.log('Connecting... to database')
+    await client.connect()
+    console.log('Connected to database')
+  } catch (e) {
+    console.error("Could not connect to database", e)
+    return client.close();
+  }
 
   return client.db('spacex-api');
 };
